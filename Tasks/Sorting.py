4294061e5e1bf6650c17f7d3f1b26258828718b3
srinathkshetty <srinathshetty24@gmail.com>
2023-01-30 21:12:22 +0530

    tasks

---
+++ Tasks/Sorting.py
@@ -0,0 +1,11 @@
+item=[5,2,3,1,4]
+for x in range(len(item)):
+    for y in range(x+1,len(item)):
+        if item[x]>item[y]:
+            print(item[x],item[y])
+            big = item[y]
+            item[y]=item[x]
+            item[x]=big
+            print(item)
+                   
+    
